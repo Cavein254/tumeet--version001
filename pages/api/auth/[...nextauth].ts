53b0f82e04c3c3499bd7ecaa2c28803eb2f05f9a
johnmuthua <caveomuthama@gmail.com>
2022-11-10 08:48:41 +0300

    feat:Added a docker file

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "../../../lib/prismadb";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin";
+      return token;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
