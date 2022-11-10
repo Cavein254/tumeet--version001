9f5cecce5a3336cdd80b771e64e5828da847bf51
johnmuthua <caveomuthama@gmail.com>
2022-11-10 15:02:48 +0300

    feat:added dropdown

--- components/header1.tsx
+++ components/header1.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 function Header1() {
+  const [showDrop, setShowDrop] = useState(false);
+  const handleShowDrop = () => {
+    setShowDrop(!showDrop);
+  };
   return (
     <div className="dark:bg-gray-700 dark:text-white py-4 w-screen gray-400 shadow-xl border-b-4 border-emerald-500">
       <div className="flex flex-row justify-evenly relative">
@@ -13,6 +18,9 @@ function Header1() {
         </div>
         <div className="flex flex-row justify-between">
           <span>sun</span>
+          <button className="bt" onClick={handleShowDrop}>
+            Drop
+          </button>
           <button className="btn bg-white text-black font-normal dark:bg-black dark:text-white dark:outline-1 dark:outline-white outline-1 ">
             Login
           </button>
