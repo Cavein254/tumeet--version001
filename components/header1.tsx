bcab5eef9d29aab637dadbbca498c0d84df71b37
johnmuthua <caveomuthama@gmail.com>
2022-11-10 13:59:18 +0300

    chore:created header itms

--- components/header1.tsx
+++ components/header1.tsx
@@ -1,7 +1,23 @@
 function Header1() {
   return (
-    <div>
-      <h1>test</h1>
+    <div className="dark:bg-gray-700 dark:text-white py-4 w-screen gray-400 shadow-xl border-b-4 border-emerald-500">
+      <div className="flex flex-row justify-evenly relative">
+        <div>Tailwind Components</div>
+        <div className=" flex-grow">
+          <div className="flex flex-row justify-evenly invisible md:visible">
+            <li>Component</li>
+            <li>Premium</li>
+            <li>Tools</li>
+            <li>Custom Development</li>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between">
+          <span>sun</span>
+          <button className="btn bg-white text-black font-normal dark:bg-black dark:text-white dark:outline-1 dark:outline-white outline-1 ">
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
