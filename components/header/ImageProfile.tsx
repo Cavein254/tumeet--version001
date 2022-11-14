d60c705b0c32a234c20ea2f2cb74724254ddfef1
johnmuthua <caveomuthama@gmail.com>
2022-11-14 10:47:14 +0300

    feat:added bg image

--- components/header/ImageProfile.tsx
+++ components/header/ImageProfile.tsx
@@ -1,12 +1,12 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 
-const LilProfile = ({ height, width }) => {
+const LilProfile = ({ height, width, classname }) => {
   const { data: session, status } = useSession();
   return (
     <div>
       <Image
-        className="rounded-full"
+        className={classname ? classname : "rounded-full"}
         src={session?.user?.image}
         height={height ? height : 50}
         width={width ? width : 50}
