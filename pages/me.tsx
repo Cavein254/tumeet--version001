939e48046aedab47f48c1049b50a39d54a4eee55
johnmuthua <caveomuthama@gmail.com>
2023-01-23 15:39:01 +0300

    fixed darkmode css in profile

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,12 +1,9 @@
-import { useSession } from "next-auth/react"
-import Layout from "../components/layout"
+import { useSession } from "next-auth/react";
+import Layout from "../components/layout";
 
 export default function MePage() {
-  const { data } = useSession()
+  const { data } = useSession();
+  console.log(data);
 
-  return (
-    <Layout>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </Layout>
-  )
+  return <Layout>{/* <pre>{JSON.stringify(data, null, 2)}</pre> */}</Layout>;
 }
