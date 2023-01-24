58c7e7653e8eb2940e9273411e378264c1372d4c
johnmuthua <caveomuthama@gmail.com>
2023-01-24 22:21:43 +0300

    feat:added data collection feature in createinvite

--- components/CreateInvite.tsx
+++ components/CreateInvite.tsx
@@ -1,12 +1,24 @@
 import "flatpickr/dist/themes/material_green.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Flatpickr from "react-flatpickr";
 import ImageProfile from "./header/ImageProfile";
 
 function CreateInvite() {
-  const [date, setDate] = useState();
-  console.log(date);
+  const [date, setDate] = useState(Date.now());
+  const initialState = {
+    title: "",
+    description: "",
+    deadline: date,
+    inviteUrl: "",
+  };
+  const [data, setData] = useState(initialState);
+  let remaining_time = initialState.deadline - date;
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: [e.target.value] });
+  };
+  useEffect(() => {}, [date]);
+  console.log(data);
   return (
     <div className="mx-12 overflow-hidden">
       <div className="m2 p-2 shadow-2xl border-2 rounded-lg border-b-2 border-emerald-300 hover:shadow-2xl hover:shadow-gray-500 dark:bg-gray-800 dark:hover:shadow-emerald-500">
@@ -31,6 +43,19 @@ function CreateInvite() {
                 name="title"
                 placeholder="Title of the invitation"
                 className="font-bold text-lg leading-4 pt-2 px-2 w-full rounded-md focus:ring-transparent"
+                value={data.title}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="mt-2">
+              <input
+                type="text"
+                name="inviteUrl"
+                placeholder="The url of the invitation"
+                className="font-bold text-lg leading-4 pt-2 px-2 w-full rounded-md focus:ring-transparent"
+                value={data.inviteUrl}
+                onChange={handleChange}
               />
             </div>
             <div className="mt-2 ">
@@ -39,6 +64,8 @@ function CreateInvite() {
                 placeholder="Detailed description of the invite for example 'Need help creating a modal in react using redux. I have managed to ....."
                 name="description"
                 className="font-light text-sm pt-2 border-b-2  px-2 w-full rounded-md"
+                value={data.description}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -47,15 +74,16 @@ function CreateInvite() {
               <div className="font-bold">
                 Enter the date and time of the meet
               </div>
+              <div>{remaining_time}</div>
             </div>
             <div>
               <div className="font-bold rounded-md">
                 <Flatpickr
                   data-enable-time
+                  name="deadline"
                   value={date}
-                  onChange={([date]) => {
-                    setDate(date);
-                  }}
+                  onChange={(e) => setDate(date)}
+                  className="rounded-md px-2 my-2"
                 />
               </div>
               <div className="mt-2">
