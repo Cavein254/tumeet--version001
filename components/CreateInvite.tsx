84a462f678476b58e3f82db66038fb1fb9ddd9f6
johnmuthua <caveomuthama@gmail.com>
2023-01-24 17:04:01 +0300

    added react-flatpickr to assist with dates

--- components/CreateInvite.tsx
+++ components/CreateInvite.tsx
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
+import Datetime from "react-datetime";
+import "flatpickr/dist/themes/material_green.css";
+
+import Flatpickr from "react-flatpickr";
+
 function CreateInvite() {
+  const [date, setDate] = useState();
+  console.log(typeof date);
   return (
     <div>
-      <div className="m-2 p-2 shadow-lg rounded-lg border-b-2 border-emerald-300 md:w-2/6 hover:shadow-2xl hover:shadow-gray-500 dark:bg-gray-800 dark:hover:shadow-emerald-500">
+      <Flatpickr
+        data-enable-time
+        value={date}
+        onChange={([date]) => {
+          setDate(date);
+        }}
+      />
+      <div className="m-2 p-2 shadow-lg rounded-lg border-b-2 border-emerald-300 hover:shadow-2xl hover:shadow-gray-500 dark:bg-gray-800 dark:hover:shadow-emerald-500">
         <div>
           <div className="flex flex-row justify-between p-2 border-b-2 border-gray-300 items-center">
             <div></div>
@@ -15,17 +30,22 @@ function CreateInvite() {
             </div>
           </div>
           <div>
-            <h1 className="font-bold text-lg leading-4 pt-2 px-2">
-              Lorem ipsum dolor, sit amet con. Lorem ipsum dolor, sit amet con
-            </h1>
+            <div className="flex flex-1">
+              <h1 className="font-bold text-lg leading-4 pt-2 px-2">
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Title of the invitation"
+                />
+              </h1>
+            </div>
           </div>
           <div className="font-light text-sm pt-2 border-b-2 border-gray-300 px-2">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
-              libero perspiciatis! Quis sit molestias nemo ipsam? Quis quidem
-              voluptas reprehenderit deserunt ex qui, labore ducimus
-              accusantium, explicabo unde nemo fuga.
-            </p>
+            <textarea
+              rows={8}
+              placeholder="Detailed description of the invite for example 'Need help creating a modal in react using redux. I have managed to ....."
+              name="description"
+            />
           </div>
           <div className="flex flex-row justify-between items-center px-2">
             <div>
@@ -33,10 +53,7 @@ function CreateInvite() {
               <div className="text-xs font-extralight">Applicants</div>
             </div>
             <div>
-              <div className="text-sm">30 min Remaining</div>
-              <div className="text-xs font-extralight">
-                Created 12 hours ago
-              </div>
+              <div className="text-xs font-extralight"></div>
             </div>
           </div>
         </div>
