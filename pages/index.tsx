b00213c7cb5f4cf78d67552cefb723cf6a014160
johnmuthua <caveomuthama@gmail.com>
2023-05-02 19:29:52 +0300

    Add to do's

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,3 +7,24 @@ export default function IndexPage() {
     </Layout>
   );
 }
+//TODO: Fix scrollbars
+//TODO: Add container to overall layout
+//TODO: Add get all invites to main page
+//TODO: Add protected route to
+  // 1. My Invites
+  // 2. Drafts
+  // 3. api getway
+//TODO: Fix onclick picture icon appearance
+//TODO: Create Links to routes
+//TODO: Fix Darkmode on profile not working as expected
+//TODO: Add working onclick links to website on profile
+//TODO: Add github 
+//TODO: Add mailto functionality to email
+//TODO: Create functionality add to list for favorites
+//TODO: Add functionality to message button
+//TODO: Add About me click to expand
+//TODO: Add Hire Me functionality with a quote and message pop-up
+//TODO: Add profile views
+//TODO: Add profile loves
+//TODO: Add Availability option
+//TODO: Timezone functionality?
