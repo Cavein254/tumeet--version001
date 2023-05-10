a21779f98eac5cbf72a9a258040c471bfee42717
johnmuthua <caveomuthama@gmail.com>
2023-05-10 12:35:01 +0300

    Add invites to landing page

--- components/main/Invitations.tsx
+++ components/main/Invitations.tsx
@@ -2,6 +2,7 @@ import Invitation from "./Invitation";
 
 function Invitations() {
   return (
+    
     <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-0">
       <Invitation />
       <Invitation />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Layout from "../components/layout";
+import Invitation from "../components/main/Invitation";
 
 export default function IndexPage({invites}) {
-  console.log(invites);
+  const all_invites = invites.map(
+    (invite) => (
+      <Invitation key={invites.id} data={invite}/>
+    )
+  )
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-0">
+        {all_invites}
+      </div>
     </Layout>
   );
 }
