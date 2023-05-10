4b26f0c450c1972484b72ac1671bbdb5a06d7249
johnmuthua <caveomuthama@gmail.com>
2023-05-10 11:23:38 +0300

    Add colors

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -41,15 +41,15 @@ function Profile() {
           <div className="md:flex md:flex-row justify-between p-2 ">
             <div className="md:w-1/2 md:mr-4 bg-blue-50 p-2 rounded-md">
               <div className="border-b-gray-200 border-2 border-blue-50">
-                <h2 className="text-lg font-bold  ">Experience</h2>
-                <h4>
+                <h2 className="text-lg font-bold dark:text-blue-700 ">Experience</h2>
+                <h4 className="dark:text-blue-700">
                   I special in UX/UI design, brand strategy, and webflow
                   development.{" "}
                 </h4>
               </div>
               <div className="mt-2 border-2 border-b-gray-200  border-blue-50">
-                <h3 className="text-lg font-bold  ">About Me</h3>
-                <p>
+                <h3 className="text-lg font-bold dark:text-blue-700">About Me</h3>
+                <p className="dark:text-blue-700">
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                   Neque ab repellendus aspernatur vero, iusto quasi, est
                   exercitationem iure pariatur dolore magnam mollitia quis ipsam
@@ -58,11 +58,11 @@ function Profile() {
 
                 <div className="flex flex-row items-center text-lg font-bold hover:text-blue-700">
                   <div>
-                    <h6>Read More </h6>
+                    <h6 className='text-blue-700'>Read More </h6>
                   </div>
                   <div className="ml-2">
                     <span>
-                      <BsArrowBarDown size={16} className="font-bold" />
+                      <BsArrowBarDown size={16} className="font-bold text-blue-700" />
                     </span>
                   </div>
                 </div>
@@ -72,46 +72,46 @@ function Profile() {
             <div className="md:w-1/2 p-2 bg-blue-50 rounded-md">
               <div className="border-2 border-b-gray-200  border-blue-50">
                 <div className="border-2 border-b-gray-200  border-blue-50 pb-4">
-                  <h3 className="text-lg font-semibold">Skills</h3>
+                  <h3 className="text-lg font-semibold dark:text-blue-800">Skills</h3>
                   <div>
-                    <span className="border-2 px-2 py-1 mr-2 border-black font-bold rounded-lg">
+                    <span className="border-2 px-2 py-1 mr-2 border-black font-bold rounded-lg dark:text-black">
                       Photoshop
                     </span>
-                    <span className="border-2 px-2 py-1  border-black font-bold rounded-lg">
+                    <span className="border-2 px-2 py-1  border-black font-bold rounded-lg dark:text-black">
                       Instagram
                     </span>
                   </div>
                 </div>
                 <div>
                   <div className="border-2 border-b-gray-200  border-blue-50">
-                    <h3 className="text-lg ">Location</h3>
-                    <span className="font-semibold">Nikaragwa, Spain</span>
+                    <h3 className="text-lg dark:text-blue-800">Location</h3>
+                    <span className="font-semibold dark:text-blue-800">Nikaragwa, Spain</span>
                   </div>
                   <div className="border-2 border-b-gray-200  border-blue-50">
-                    <h3 className="text-lg ">Website</h3>
+                    <h3 className="text-lg dark:text-blue-800">Website</h3>
                     <div className="flex flex-row items-center font-semibold">
                       <div>
-                        <span>www.olddogs.com</span>
+                        <span className='dark:text-blue-800'>www.olddogs.com</span>
                       </div>
-                      <div>
+                      <div className='dark:text-blue-800'>
                         <FiArrowUpRight />
                       </div>
                     </div>
                   </div>
                   <div className="border-2 border-b-gray-200 border-blue-50">
-                    <h3 className="text-lg">Email</h3>
+                    <h3 className="text-lg dark:text-blue-800">Email</h3>
                     <div className="flex flex-row items-center font-semibold">
                       <div>
-                        <span>olddogs@gmail.com</span>
+                        <span className='dark:text-blue-800'>olddogs@gmail.com</span>
                       </div>
-                      <div>
+                      <div className='dark:text-blue-800'>
                         <FiArrowUpRight />
                       </div>
                     </div>
                   </div>
                   <div className="flex flex-col md:flex-row items-center pt-2 w-max justify-center">
                     <div className="items-center">
-                      <button className="btn border-2 border-black font-bold mr-2 bg-white mb-2">
+                      <button className="btn border-2 border-black font-bold mr-2 bg-white mb-2 dark:text-black">
                         Add To List
                       </button>
                     </div>
