e441063df11a179ab01f11a5fe7edfabc37156f1
johnmuthua <caveomuthama@gmail.com>
2023-05-11 17:20:11 +0300

    Add styles

--- components/main/InviteCreate.tsx
+++ components/main/InviteCreate.tsx
@@ -2,13 +2,13 @@
 function InviteCreate() {
   return (
       <div className="flex justify-center px-2">
-        <form className="flex flex-col bg-gray-100 mt-4 px-4 py-4 rounded-md min-w-full shadow-lg hover:shadow-gray-400 dark:hover:shadow-green-300">
+        <form className="flex flex-col bg-gray-100 mt-4 px-4 py-4 rounded-md min-w-full shadow-lg hover:shadow-gray-400 dark:hover:shadow-green-300 border-b-4 border-emerald-500">
             <h1 className="text-center text-green-600 text-2xl font-extrabold">Enter Invitation Details</h1>
             <div className="pt-2">
-                <input placeholder="Enter Title" className="w-[100%] rounded-md px-2 font-thin text-xl "/>
+                <input placeholder="Enter Title" className="w-[100%] rounded-md px-2 font-thin text-xl " required/>
             </div>
             <div>
-                <textarea placeholder="Enter Description" rows={6} className="w-[100%] mt-4 rounded-md px-1 font-thin text-xl"/>
+                <textarea placeholder="Enter Description" rows={6} className="w-[100%] mt-4 rounded-md px-1 font-thin text-xl" required/>
             </div>
             <div className="flex justify-end">
             <div className="items-center">
