3cb21fdb5313eda956741cfdf6801310eabe905c
johnmuthua <caveomuthama@gmail.com>
2023-05-11 19:30:24 +0300

    Fix: create popup and fix links

--- components/header/Profile.tsx
+++ components/header/Profile.tsx
@@ -1,39 +1,28 @@
+import Link from "next/link";
+
 function Profile() {
   return (
-    <ul className="fixed float-left dropdown-menu min-w-max items-center  bg-white text-base py-2 list-none text-left rounded-lg shadow-lg mt-6 m-0 bg-clip-padding border-none ">
-      <li>
-        <a
-          className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-          href="#"
-        >
-          Action
-        </a>
-      </li>
-      <li>
-        <a
-          className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-          href="#"
-        >
-          Action
-        </a>
-      </li>
-      <li>
-        <a
-          className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-          href="#"
-        >
-          Action
-        </a>
-      </li>
-      <li>
-        <a
-          className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-          href="#"
-        >
-          Action
-        </a>
-      </li>
-    </ul>
+    <div className="z-50 right-0 left-[68%] top-[6%] md:left-[75%] bg-gray-200 fixed dark:bg-black w-[100%] rounded-md shadow-lg flex flex-col justify-center">
+      <div className="mx-2 my-4 px-2 py-2 w-[100%] justify-center">
+        <div>
+          <Link href={'/create-invite'} className="md:text-xl dark:text-white  hover:text-green-600">Create Invite</Link>
+        </div>
+        
+        <div className="mt-2">
+          <Link href={'/create-invite'} className="md:text-xl dark:text-white  hover:text-green-600">Current Invites</Link>
+        </div>
+        <div className="mt-2">
+          <Link href={'/create-invite'} className="md:text-xl dark:text-white  hover:text-green-600">Past Invites</Link>
+        </div>
+        <div className="mt-2">
+          <Link href={'/create-invite'} className="md:text-xl dark:text-white  hover:text-green-600">History</Link>
+        </div>
+        <hr className="w-[100%] -ml-[1.1rem] border-2 border-green-400 mt-4"/>
+        <div className="mt-2">
+          <Link href={'/me'} className="md:text-xl dark:text-white  hover:text-green-600">My Profile</Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
