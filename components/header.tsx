854e529f7352ca1263ecc5b4da70062a05289aaa
johnmuthua <caveomuthama@gmail.com>
2023-05-11 16:18:31 +0300

    Add change header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,84 +1,72 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
+import { FaLock } from "react-icons/fa";
+import ThemeToggler from "./header/ThemeToggler";
+
 import Link from "next/link";
-
-// The approach used in this component shows how to build a sign in and sign out
-// component that works on pages which support both client and server side
-// rendering, and avoids any flash incorrect content on initial page load.
-export default function Header() {
+import Profile from "./header/Profile";
+function Header1() {
   const { data: session, status } = useSession();
-  const loading = status === "loading";
 
+  const [showDrop, setShowDrop] = useState(false);
+  const handleShowDrop = () => {
+    setShowDrop(!showDrop);
+  };
   return (
-    <header>
-      <noscript>
-        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
-      </noscript>
-      <div>
-        <p>
-          {!session && (
-            <>
-              <span>You are not signed in</span>
-              <a
-                href={`/api/auth/signin`}
-                onClick={(e) => {
-                  e.preventDefault();
-                  signIn();
-                }}
-              >
-                Sign in
-              </a>
-            </>
-          )}
-          {session?.user && (
-            <>
-              {session.user.image && (
-                <span
-                  style={{ backgroundImage: `url('${session.user.image}')` }}
+    <div className="max-w-screen h-24 min-w-min">
+      <div className=" flex-wrap max-w-screen dark:bg-gray-700 dark:text-white py-4  gray-400 shadow-xl border-b-4 border-emerald-500 px-4">
+        <div className="flex flex-row justify-evenly items-center">
+          <div>
+            <span className="text-5xl font-extrabold text-green-600">TU</span><span className="italic font-extralight text-3xl dark:text-white">meet</span>
+          </div>
+          <nav className=" flex-grow">
+            <ul className="flex flex-row justify-evenly invisible md:visible">
+              <li><Link href='/create-profile'>Create Profile</Link></li>
+              <li><Link href='/create-invite'>Create Invite</Link></li>
+              <li>Premium</li>
+              <li>Tools</li>
+              <li>Custom Development</li>
+            </ul>
+          </nav>
+          <div className="flex flex-wrap">
+            <div>
+              <ThemeToggler />
+            </div>
+
+            {showDrop && <Profile />}
+            {!session ? (
+              <div>
+                <button className="btn ml-2  border-emerald-500 hover:bg-black bg-white font-normal dark:bg-black dark:text-white dark:outline-1 dark:outline-white border-2 ">
+                  <a
+                    href={`/api/auth/signin`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signIn();
+                    }}
+                  >
+                    <FaLock
+                      // size={16}
+                      className="hover:text-emerald-500"
+                    />
+                  </a>
+                </button>
+              </div>
+            ) : (
+              <button onClick={handleShowDrop}>
+                <Image
+                  className="rounded-full"
+                  src={session?.user?.image}
+                  height={50}
+                  width={50}
                 />
-              )}
-              <span>
-                <small>Signed in as</small>
-                <br />
-                <strong>{session.user.email ?? session.user.name}</strong>
-              </span>
-              <a
-                href={`/api/auth/signout`}
-                onClick={(e) => {
-                  e.preventDefault();
-                  signOut();
-                }}
-              >
-                Sign out
-              </a>
-            </>
-          )}
-        </p>
+              </button>
+            )}
+          </div>
+        </div>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/client">Client</Link>
-          </li>
-          <li>
-            <Link href="/server">Server</Link>
-          </li>
-          <li>
-            <Link href="/protected">Protected</Link>
-          </li>
-          <li>
-            <Link href="/api-example">API</Link>
-          </li>
-          <li>
-            <Link href="/admin">Admin</Link>
-          </li>
-          <li>
-            <Link href="/me">Me</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    </div>
   );
 }
+
+export default Header1;
--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,6 @@ import { useSession } from "next-auth/react";
 import type { ReactNode } from "react";
 import Footer from "./footer";
 import Header from "./header";
-import Header1 from "./header1";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const { data: session, status } = useSession();
@@ -10,7 +9,6 @@ export default function Layout({ children }: { children: ReactNode }) {
 
   return (
     <div className="w-screen">
-      <Header1 />
       <Header />
       <main>{children}</main>
       <Footer />
